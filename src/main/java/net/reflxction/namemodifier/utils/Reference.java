3105be45a9f54ebe01498daf29940994dc3e7df4
ReflxctionDev <aloosh.kasasbeh@gmail.com>
2019-01-22 13:35:31 +0200

    Update versioning

--- src/main/java/net/reflxction/namemodifier/utils/Reference.java
+++ src/main/java/net/reflxction/namemodifier/utils/Reference.java
@@ -27,7 +27,7 @@ public class Reference {
     public static final String NAME = "Name Modifier";
 
     // Mod version
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     // Minecraft versions that the mod works on
     public static final String ACCEPTED_VERSIONS = "[1.8.9]";
